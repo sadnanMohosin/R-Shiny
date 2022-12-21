fbfb3e2737fa8649ce13321d494a891db39ba9f7
sadnanMohosin <smsadnan15@gmail.com>
2022-12-22 00:44:23 +0600

    navbar

---
+++ 5.Multi Page App Layouts/2.Navbar panel/ui.R
@@ -0,0 +1,43 @@
+navbarPage("Navbar Example",
+           tabPanel("Inputs",
+                    selectInput(inputId = "xSelector",label = "Select the x axis", choices = xAxisChoices ),
+                    selectInput(inputId = "ySelector",label = "Select the y axis", choices = yAxisChoices ),
+                    selectInput("cylSelector",label = "Select a cylinder",choices = cylinderChoices),
+                    checkboxInput("showtitle",label= "Check to enter title",value= FALSE),
+                    conditionalPanel(condition = "input.showtitle==true",
+                                     textInput("title",label = "Enter a plot title",placeholder = "Title")),
+                    actionButton("refreshPlot",label = "Refresh")
+                    ),
+           navbarMenu("Outputs",
+                      tabPanel("Plot",
+                               plotOutput("p1")
+                               ),
+                      tabPanel("Data",
+                               DTOutput("d1"))
+                      )
+           )
+
+# fluidPage(
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput(inputId = "xSelector",label = "Select the x axis", choices = xAxisChoices ),
+#       selectInput(inputId = "ySelector",label = "Select the y axis", choices = yAxisChoices ),
+#       selectInput("cylSelector",label = "Select a cylinder",choices = cylinderChoices),
+#       checkboxInput("showtitle",label= "Check to enter title",value= FALSE),
+#       conditionalPanel(condition = "input.showtitle==true",
+#                        textInput("title",label = "Enter a plot title",placeholder = "Title")),
+#       actionButton("refreshPlot",label = "Refresh")
+#       
+#     ),
+#     mainPanel(
+#       tabsetPanel(
+#         tabPanel("Plot",plotOutput("p1")),
+#         tabPanel("Data",dataTableOutput("d1"))
+#       
+#       
+#     )
+#   )
+# 
+# 
+#   
+# ))
