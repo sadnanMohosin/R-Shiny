b3a358a6192bb0c362477ca8644c89d33e6dc1b3
sadnanMohosin <smsadnan15@gmail.com>
2022-12-22 01:02:52 +0600

    navlist

---
+++ 5.Multi Page App Layouts/3.NavList Panel/ui.R
@@ -0,0 +1,54 @@
+
+fluidPage(
+  navlistPanel("Navlist Example",
+               tabPanel("Inputs",
+                        selectInput(inputId = "xSelector",label = "Select the x axis", choices = xAxisChoices ),
+                        selectInput(inputId = "ySelector",label = "Select the y axis", choices = yAxisChoices ),
+                        selectInput("cylSelector",label = "Select a cylinder",choices = cylinderChoices),
+                        checkboxInput("showtitle",label= "Check to enter title",value= FALSE),
+                        conditionalPanel(condition = "input.showtitle==true",
+                                         textInput("title",label = "Enter a plot title",placeholder = "Title")),
+                        actionButton("refreshPlot",label = "Refresh")
+                        ),
+               tabPanel("Plot",
+                        plotOutput("p1")
+               ),
+               tabPanel("Data",
+                        DTOutput("d1")
+                        )
+               )
+)
+
+# navbarPage("Navbar Example",
+#            tabPanel("Inputs",
+#                     
+#                     ),
+#            navbarMenu("Outputs",
+#                       
+#                       )
+#            )
+
+# fluidPage(
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput(inputId = "xSelector",label = "Select the x axis", choices = xAxisChoices ),
+#       selectInput(inputId = "ySelector",label = "Select the y axis", choices = yAxisChoices ),
+#       selectInput("cylSelector",label = "Select a cylinder",choices = cylinderChoices),
+#       checkboxInput("showtitle",label= "Check to enter title",value= FALSE),
+#       conditionalPanel(condition = "input.showtitle==true",
+#                        textInput("title",label = "Enter a plot title",placeholder = "Title")),
+#       actionButton("refreshPlot",label = "Refresh")
+#       
+#     ),
+#     mainPanel(
+#       tabsetPanel(
+#         tabPanel("Plot",plotOutput("p1")),
+#         tabPanel("Data",dataTableOutput("d1"))
+#       
+#       
+#     )
+#   )
+# 
+# 
+#   
+# ))
